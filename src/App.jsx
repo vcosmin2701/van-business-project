cf1be69ddbe2ba432a374f53d7af2fa9a29b0b3a
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-25 20:15:59 +0300

    Home page and about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import About from './pages/About.jsx';
+import Home from './pages/Home.jsx'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
