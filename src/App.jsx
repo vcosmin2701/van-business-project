cd1859c9758ec4771df931ec77cda828fa4dfd36
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-26 00:07:58 +0300

    Adding Layout and Outlet for the page

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,19 @@ import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Vans from "./pages/Vans";
 import VanDetails from "./pages/VanDetails";
+import Layout from "./components/Layout";
 import "./server";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetails />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
